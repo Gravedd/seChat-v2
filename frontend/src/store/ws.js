4aa69a04fac91911ddd903180e628d50db09e923
Gravedd <troperread@gmail.com>
2022-05-10 16:11:13 +0400

    Update ws.js
    reload if ws connetcion error

--- frontend/src/store/ws.js
+++ frontend/src/store/ws.js
@@ -22,8 +22,9 @@ export default {
     actions:{
         async connectws(context) {
             context.state.websocket = new WebSocket(context.getters.websocketsurl);
+            let connectioncount = 0;
             context.state.websocket.onerror = function (error) {
-                showalert('Ошибка!', 'Не удачное подключение к websockets');
+                    location.reload()
             }
             context.state.websocket.onmessage = event => {
                 let response = JSON.parse(event.data);
